e3c8c19d46183d881756aa2f99b856d817cb0c1a
Ahty_M <motejunzhuanyong@163.com>
2020-02-12 10:45:20 +0800

    添加 机票头部和列表组件

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,13 +1,61 @@
 <template>
-  <div>机票列表</div>
+    <section class="contianer">
+        <el-row  type="flex" justify="space-between">
+
+            <!-- 顶部过滤列表 -->
+            <div class="flights-content">
+                <!-- 过滤条件 -->
+                <div>
+                    
+                </div>
+                
+                <!-- 航班头部布局 -->
+             
+                    <FlightsListHead/>
+               
+                
+                
+                <!-- 航班信息 -->
+                <FlightsItem/>
+            </div>
+
+            <!-- 侧边栏 -->
+            <div class="aside">
+                <!-- 侧边栏组件 -->
+            </div>
+        </el-row>
+    </section>
 </template>
 
 <script>
-export default {
 
+import moment from "moment";
+import FlightsListHead from '../../components/air/flightsListHead' //头部
+import FlightsItem from '../../components/air/flightsItem' //列表
+export default {
+    data(){
+        return {
+            
+        }
+    },
+    components:{
+        FlightsListHead,FlightsItem
+    }
 }
 </script>
 
-<style>
+<style scoped lang="less">
+    .contianer{
+        width:1000px;
+        margin:20px auto;
+    }
 
+    .flights-content{
+        width:745px;
+        font-size:14px;
+    }
+
+    .aside{
+        width:240px;
+    } 
 </style>
