1597fb9c7f1397ec5d647dd3cdf479c3f27e3af3
Ahty_M <motejunzhuanyong@163.com>
2020-02-12 18:21:58 +0800

    添加分页

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,76 +1,114 @@
 <template>
-    <section class="contianer">
-        <el-row  type="flex" justify="space-between">
+  <section class="contianer">
+    <el-row type="flex" justify="space-between">
+      <!-- 顶部过滤列表 -->
+      <div class="flights-content">
+        <!-- 过滤条件 -->
+        <div></div>
 
-            <!-- 顶部过滤列表 -->
-            <div class="flights-content">
-                <!-- 过滤条件 -->
-                <div>
-                    
-                </div>
-                
-                <!-- 航班头部布局 -->
-             
-                    <FlightsListHead/>
-               
-                
-                
-                <!-- 航班信息 -->
-                <FlightsItem v-for="(item,index) in dataList" :key="index" :data='item'/>
-            </div>
+        <!-- 航班头部布局 -->
 
-            <!-- 侧边栏 -->
-            <div class="aside">
-                <!-- 侧边栏组件 -->
-            </div>
-        </el-row>
-    </section>
+        <FlightsListHead />
+
+        <!-- 航班信息 -->
+        <FlightsItem v-for="(item,index) in dataList" :key="index" :data="item" />
+        <!-- <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page.sync="pageIndex"
+          :page-sizes="[5, 10, 15, 20]"
+          :page-size="pageSize"
+          layout="sizes, prev, pager, next"
+          :total="total"
+        ></el-pagination>-->
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="pageIndex"
+          :page-sizes="[5, 10, 15, 20]"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+        ></el-pagination>
+      </div>
+
+      <!-- 侧边栏 -->
+      <div class="aside">
+        <!-- 侧边栏组件 -->
+      </div>
+    </el-row>
+  </section>
 </template>
 
 <script>
-
 import moment from "moment";
-import FlightsListHead from '../../components/air/flightsListHead' //头部
-import FlightsItem from '../../components/air/flightsItem' //列表
+import FlightsListHead from "../../components/air/flightsListHead"; //头部
+import FlightsItem from "../../components/air/flightsItem"; //列表
 export default {
-    data(){
-        return {
-            //航班信息 （info,flights,options,total）
-            flightsData:{}, 
-            dataList:[]
-        }
-    },
-    components:{
-        FlightsListHead,FlightsItem
-    },
-    mounted(){
-        this.$axios({
-            url:'/airs',
-            params:this.$route.query
-        }).then(res=>{
-            console.log(res);
-          this.flightsData=res.data
-          this.dataList=res.data.flights
-         
-           
-            
-        })
+  data() {
+    return {
+      //航班信息 （info,flights,options,total）
+      flightsData: {},
+
+      pageIndex: 2, //当前的页数
+      pageSize: 5, //当前的页码
+      total: 0 //页数
+    };
+  },
+  components: {
+    FlightsListHead,
+    FlightsItem
+  },
+  mounted() {
+    this.$axios({
+      url: "/airs",
+      params: this.$route.query
+    }).then(res => {
+      console.log(res);
+      this.flightsData = res.data;
+      //   this.total = res.data.total;
+      this.total = this.flightsData.total;
+    });
+  },
+  computed: {
+    dataList() {
+      if (!this.flightsData.flights) {
+        return [];
+      }
+      const arr = this.flightsData.flights.slice(
+        (this.pageIndex - 1) * this.pageSize,
+        this.pageIndex * this.pageSize
+      );
+      return arr;
+
     }
-}
+    
+  },
+  methods: {
+    //每页有多少条
+    handleSizeChange(index) {
+      this.pageSize = index;
+    },
+    //当前是哪一页
+    handleCurrentChange(index) {
+      this.pageIndex = index;
+    }
+  }
+};
 </script>
 
 <style scoped lang="less">
-    .contianer{
-        width:1000px;
-        margin:20px auto;
-    }
+.contianer {
+  width: 1000px;
+  margin: 20px auto;
+}
 
-    .flights-content{
-        width:745px;
-        font-size:14px;
-    }
+.flights-content {
+  width: 745px;
+  font-size: 14px;
+}
 
-    .aside{
-        width:240px;
-    } 
+.aside {
+  width: 240px;
+}
 </style>
